673c942633b53f1c14ed89d592b41e7eca87e719
Javier Martínez - Junior Software Engineer <jmartinez@develsystems.com>
2022-04-12 17:54:54 -0600

    Obtener heroes y router-outlet agregado

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,32 +1,22 @@
 import { MarvelService } from './../../services/marvel.service';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs'
+import { Observable } from 'rxjs';
+import { RespuestaAPIMarvel, Result } from '../../interfaces/IApiMarvelRespuesta';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
-export class InicioComponent implements OnInit  {
+export class InicioComponent implements OnInit {
+  newCaracteres: Result[] = [];
 
-newCaracteres : any[] = [];
+  constructor(private marvel: MarvelService) {}
 
-  constructor( private marvel : MarvelService ) {
-
-
-    this.marvel.getCharactersv2().subscribe(res => {
-      console.log(res);
-    })
-
-
-
-   }
-
-  ngOnInit() {
-    this.marvel.getCharactersv2().subscribe(res => {
+  ngOnInit(): void {
+    this.marvel.getCharactersv2().subscribe((res: RespuestaAPIMarvel) => {
+      this.newCaracteres = res.data.results;
       console.log(res);
     })
   }
-
-  }
-
+}
--- src/app/services/marvel.service.ts
+++ src/app/services/marvel.service.ts
@@ -1,61 +1,39 @@
-import { Injectable } from '@angular/core';// esto quiere decir que este servicio se puede inyectar en otro componente
-import { HttpClient,HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core'; // esto quiere decir que este servicio se puede inyectar en otro componente
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { RespuestaAPIMarvel } from '../interfaces/IApiMarvelRespuesta';
 
-@Injectable({// a veces este decorador viene vacio y cuando vinen vacion podremos crear una uunica instancia
-  providedIn: 'root'//esta es una forma automatica de importar servicios
+@Injectable({
+  // a veces este decorador viene vacio y cuando vinen vacion podremos crear una uunica instancia
+  providedIn: 'root', //esta es una forma automatica de importar servicios
 })
 export class MarvelService {
+  constructor(private http: HttpClient) {}
+  getQuery(query: string) {
+    const url = `https://gateway.marvel.com:443/v1/public/${query}`;
+    //getCharacters() {
 
-constructor(private http :HttpClient) {
-
-}
-getQuery( query : string ){
-
-  const url = `https://gateway.marvel.com:443/v1/public/${ query }`
-  //getCharacters() {
-
-  const headers = new HttpHeaders({
-      'ts' :'1',
-      'hash' : '810769ff9c72785b2d75b7f6f3aad258',
-      'apikey': 'e57edbf4df58748839ebec5783eb9672'
-
-
-  });
-  return this.http.get(url, { headers });
+    const headers = new HttpHeaders({
+      ts: '1',
+      hash: '810769ff9c72785b2d75b7f6f3aad258',
+      apikey: 'e57edbf4df58748839ebec5783eb9672',
+    });
+    return this.http.get(url, { headers });
     //const KEY = 'e57edbf4df58748839ebec5783eb9672';
 
     //this.http.get('https://gateway.marvel.com:443/v1/public/characters?apikey='+ KEY, {headers});
-
   }
 
-getCharacters(){
+  getCharacters() {
+    return this.getQuery('?apikey=e57edbf4df58748839ebec5783eb9672').pipe(
+      map((data) => ['characters'])
+    );
+  }
 
+  getCharactersv2() {
+    let misParametros = new HttpParams().set('ts', 1).set('hash', "810769ff9c72785b2d75b7f6f3aad258").set('apikey', "e57edbf4df58748839ebec5783eb9672");
 
-  return this.getQuery('?apikey=e57edbf4df58748839ebec5783eb9672')
-          .pipe( map( data => ['characters']));
-
+    return this.http.get<RespuestaAPIMarvel>(`http://gateway.marvel.com/v1/public/characters`, { params: misParametros })
+  }
 }
-
-getCharactersv2() {
-
-  let params = new HttpParams();
-
-  params.append('ts', 1);
-  params.append('hash', '810769ff9c72785b2d75b7f6f3aad258');
-  params.append('apikey', 'e57edbf4df58748839ebec5783eb9672')
-
-  // return this.http.get(`http://gateway.marvel.com/v1/public/characters`, {params:params})
-  return this.http.get(`http://gateway.marvel.com/v1/public/characters?ts=1&hash=810769ff9c72785b2d75b7f6f3aad258&apikey=e57edbf4df58748839ebec5783eb9672`)
-
-}
-
-
-}
-
-
-
-
-
-
